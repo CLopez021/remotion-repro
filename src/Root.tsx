a79616180ff11cc111e40bd3f8f045835f54e1ba
None None <winzlohtml@gmail.com>
2025-04-02 16:49:10 -0500

    working but lambda seems to be broken

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,6 @@
 import { Composition } from "remotion";
 import { MyComposition } from "./compositions/MyComposition";
+import { ClipComposition } from "./compositions/ClipComposition";
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -11,6 +12,22 @@ export const RemotionRoot: React.FC = () => {
         fps={30}
         width={1280}
         height={720}
+      />
+      <Composition
+        id="ClipComp"
+        component={ClipComposition}
+        durationInFrames={3000} // 100 seconds at 30fps
+        fps={30}
+        width={1280}
+        height={720}
+        defaultProps={{
+          video_url: "",
+          clip: {
+            start_time: 0,
+            end_time: 0,
+            transcript: []
+          }
+        }}
       />
     </>
   );
