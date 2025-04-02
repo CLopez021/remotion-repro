81d310195d6ffa586ab598e255ed8f738f135cb2
None None <winzlohtml@gmail.com>
2025-04-01 21:06:15 -0500

    everything working but the studio GUI is there

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,5 @@
 import { Composition } from "remotion";
-import { MyComposition } from "./Composition";
+import { MyComposition } from "./compositions/MyComposition";
 
 export const RemotionRoot: React.FC = () => {
   return (
---
+++ src/compositions/MyComposition.tsx
@@ -0,0 +1,9 @@
+import { AbsoluteFill } from 'remotion';
+
+export const MyComposition = () => {
+  return (
+    <AbsoluteFill>
+      {/* Your composition content will go here */}
+    </AbsoluteFill>
+  );
+}; 
