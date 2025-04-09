1c5c23c4c0817661829873bf67d53bbb264e1fc0
None None <winzlohtml@gmail.com>
2025-04-08 23:37:06 -0500

    basic repro

--- src/compositions/MyComposition.tsx
+++ src/compositions/MyComposition.tsx
@@ -2,10 +2,7 @@ import { AbsoluteFill } from 'remotion';
 
 export const MyComposition = () => {
   return (
-    <AbsoluteFill style={{
-      backgroundColor: '#0000FF',
-    }}>
-      {/* Your composition content will go here */}
+    <AbsoluteFill>
     </AbsoluteFill>
   );
 }; 
